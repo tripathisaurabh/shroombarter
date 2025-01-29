420c46d6fd5f8c001091deb047a02c9cbec671d4
tripathisaurabh <tripathisaurabh332@gmail.com>
2025-01-29 05:38:14 +0530

    Updated the full Django project with recent changes

--- shroombarter/settings.py
+++ shroombarter/settings.py
@@ -63,10 +63,19 @@ WSGI_APPLICATION = 'shroombarter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+from urllib.parse import urlparse
+DATABASE_URL = os.environ.get('DATABASE_URL')
+db_info = urlparse(DATABASE_URL)
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql.psycopg2',
+        'NAME':'db',
+        'USER': db_info.username,
+        'PASSWORD': db_info.password,
+        'HOST': db_info.hostname,
+        'PORT': db_info.port,
+        'OPTIONS': {'sslmode': 'require'},
     }
 }
 
