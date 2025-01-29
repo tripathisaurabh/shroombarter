b364351de0f22c61452d53fa42269a6b50c5bd9d
tripathisaurabh <tripathisaurabh332@gmail.com>
2025-01-30 01:13:52 +0530

    Updated the full Django project with recent changes

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from django.views.static import serve
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
@@ -19,11 +22,7 @@ urlpatterns = [
     path('profile/', views.profile_view, name='profile'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve media files in production
+urlpatterns += [
+    path(f'{settings.MEDIA_URL.lstrip("/")}<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
+]
